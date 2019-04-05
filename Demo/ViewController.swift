cafa4f5139508412fa37b1569faaab4eef1f1ecb
Patrick Kaalund <pak@amsiq.com>
2019-04-05 09:46:03 +0200

    Write

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let realm = try! Realm()
+        
+        // Create
+        let person = Person()
+        person.firstName = "Tim"
+        person.lastName = "Cook"
+        person.age = 58
+        
+        // Persist
+        try! realm.write() {
+            let managedPerson = realm.create(Person.self, value: person)
+            
+            debugPrint("Wrote person has age: \(managedPerson.age)")
+        }
     }
     
 }
